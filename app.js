228e2c8cf58110bb3524be713e2fd99eb6b591f9
John U <arjorb@gmail.com>
2023-08-13 13:33:40 +0200

    being able to access the request through the browser using the get method

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('You are able to acces ths request');
+});
+
 const port = 4000;
 app.listen(port, () => {
   console.log(`starting the server on port ${port}`);
