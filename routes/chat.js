8dbe23dfce83b5237204cb65d0496da38fa05e1c
John U <arjorb@gmail.com>
2023-08-17 11:08:38 +0200

    creating a post route to add a new chat and also install the body-parser

--- routes/chat.js
+++ routes/chat.js
@@ -2,10 +2,17 @@ const express = require('express');
 const Blog = require('../models/chat');
 const route = express.Router();
 
-//getting routes
+//getting all the items from mongoDb
 route.get('/', (req, res) => {
   Blog.find()
     .then(result => res.send(result))
     .catch(err => err);
 });
 module.exports = route;
+
+// posting a new item to mongoDb
+
+route.post('/', (req, res) => {
+  const newItem = req.body;
+  console.log(newItem);
+});
